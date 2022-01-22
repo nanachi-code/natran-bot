8fcccaf56fb9e46183e95cc9707ae5883de33613
Nanachi <dinhvu2509@gmail.com>
2022-01-23 02:44:41 +0700

    add search

--- deployCommands.js
+++ deployCommands.js
@@ -4,7 +4,7 @@ const { REST } = require('@discordjs/rest')
 const { Routes } = require('discord-api-types/v9')
 
 const commands = [
-	new SlashCommandBuilder().setName('play').setDescription('Play').addStringOption(opt => opt.setName('url').setDescription('Enter YouTube url')),
+	new SlashCommandBuilder().setName('play').setDescription('Play').addStringOption(opt => opt.setName('input').setDescription('Enter YouTube url/ keyword to search for')),
 	new SlashCommandBuilder().setName('playing').setDescription('Show playlist'),
 	new SlashCommandBuilder().setName('stop').setDescription('Stop'),
 	new SlashCommandBuilder().setName('skip').setDescription('Skip'),
